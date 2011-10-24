66414a80385d445a1611938b5407455bb38c6030
Marcus Breese <mbreese@iupui.edu>
2011-10-24 14:21:01 -0700

    Reorg... not sure if needed; added -noz and -s options

---
+++ xsqutils/__init__.py
@@ -0,0 +1,154 @@
+import sys
+import collections
+
+import h5py
+
+Tag = collections.namedtuple('Tag', 'tag is_colorspace prefix')
+
+try:
+    from eta import ETA
+except:
+    ETA = None
+
+class XSQFile(object):
+    def __init__(self, fname):
+        self.fname = fname
+        self.fileobj = h5py.File(fname, 'r')
+        self._samples = []
+
+        for sample in self.fileobj:
+            if sample not in  ['RunMetadata', 'Indexing']:
+                self._samples.append(sample)
+
+        self.tags = {}
+        for tag in self.fileobj['RunMetadata']['TagDetails']:
+            t = self.fileobj['RunMetadata']['TagDetails'][tag]
+            self.tags[tag] = Tag(tag, t.attrs['IsColorPresent'][0] == 1, t.attrs['TagSequence'][0])
+
+    def close(self):
+        self.fileobj.close()
+
+    def get_samples(self):
+        return self._samples
+
+    def get_read_count(self, sample):
+        if not sample in self.fileobj:
+            raise "Invalid sample name: %s" % sample
+        count = 0
+        for region in self.fileobj[sample]:
+            count += len(self.fileobj[sample][region]['Fragments']['yxLocation'])
+        return count
+
+    def fetch(self, sample, tags=None):
+        if not tags:
+            tags = [t for t in  self.tags]
+
+        if not sample in self.fileobj:
+            raise "Invalid sample name: %s" % sample
+
+        if ETA:
+            count = 0
+            for region in self.fileobj[sample]:
+                count += len(self.fileobj[sample][region]['Fragments']['yxLocation'])
+            eta = ETA(count)
+        else:
+            eta = None
+
+        # Reads each region into memory at a time by tag. Then yields the sequences
+        # in order so that the tags are interlaced.
+        #
+        # This is slightly faster than just reading in one at a time.
+
+        n = 0
+        for i, region in enumerate(self.fileobj[sample]):
+            locations = []
+            for y, x in self.fileobj[sample][region]['Fragments']['yxLocation']:
+                locations.append((y, x))
+
+            vals = {}
+            for tag in tags:
+                vals[tag] = []
+                if self.tags[tag].is_colorspace:
+                    k = 'ColorCallQV'
+                else:
+                    k = 'BaseCallQV'
+
+                for (y, x), basequals in zip(locations, self.fileobj[sample][region][tag][k]):
+                    if eta:
+                        n += 1
+                        eta.print_status(n)
+
+                    calls = []
+                    if self.tags[tag].prefix:
+                        calls.append(self.tags[tag].prefix)
+
+                    quals = []
+                    for byte in basequals:
+                        call = str(byte & 0x03)
+                        qual = byte >> 2
+
+                        if qual == 63:
+                            call = '.'
+                            qual = 0
+
+                        calls.append(call)
+                        quals.append(qual)
+
+                    name = '%s_%s_%s' % (int(region), y, x)
+                    if len(tags) > 1:
+                        name = name + ' %s' % (tag)
+
+                    vals[tag].append((name, ''.join(calls), quals))
+
+            for i in xrange(len(locations)):
+                for tag in tags:
+                    yield(vals[tag][i])
+        if eta:
+            eta.done()
+
+    def dump(self, key, parent=None, indent=0):
+        if parent is None:
+            parent = self.fileobj
+
+        spaces = '  ' * indent
+
+        print '%s[%s]' % (spaces, key)
+
+        if 'CLASS' in parent[key].attrs and parent[key].attrs['CLASS'] == 'TABLE':
+            headers = []
+            maxsize = []
+            for name, val in parent[key].attrs.iteritems():
+                if name[:6] == 'FIELD_' and name[-5:] == '_NAME':
+                    headers.append(val)
+                    maxsize.append(len(val))
+
+            values = []
+            for child in parent[key]:
+                values.append(child)
+
+            for valset in values:
+                for i, val in enumerate(valset):
+                    if len(str(val)) > maxsize[i]:
+                        maxsize[i] = len(str(val))
+
+            sys.stdout.write(spaces + '  ')
+            sys.stdout.write(' | ')
+            for header, size in zip(headers, maxsize):
+                sys.stdout.write(header.ljust(size))
+                sys.stdout.write(' | ')
+            sys.stdout.write('\n')
+            for valset in values:
+                sys.stdout.write(spaces + '  ')
+                sys.stdout.write(' | ')
+                for val, size in zip(valset, maxsize):
+                    sys.stdout.write(str(val).ljust(size))
+                    sys.stdout.write(' | ')
+                sys.stdout.write('\n')
+
+        else:
+            if parent[key].attrs:
+                for name, val in parent[key].attrs.iteritems():
+                    print '%s  %s: %s' % (spaces, name, val)
+
+            for child in parent[key]:
+                self.dump(child, parent[key], indent + 1)
--- xsqutils/xsq.py
+++ xsqutils/xsq.py
@@ -6,161 +6,8 @@ Converts XSQ files to FASTQ format
 import os
 import sys
 import gzip
-import collections
 
-import h5py
-
-Tag = collections.namedtuple('Tag', 'tag is_colorspace prefix')
-
-try:
-    from eta import ETA
-except:
-    ETA = None
-
-
-class XSQFile(object):
-    def __init__(self, fname):
-        self.fname = fname
-        self.fileobj = h5py.File(fname, 'r')
-        self._samples = []
-
-        for sample in self.fileobj:
-            if sample not in  ['RunMetadata', 'Indexing']:
-                self._samples.append(sample)
-
-        self.tags = {}
-        for tag in self.fileobj['RunMetadata']['TagDetails']:
-            t = self.fileobj['RunMetadata']['TagDetails'][tag]
-            self.tags[tag] = Tag(tag, t.attrs['IsColorPresent'][0] == 1, t.attrs['TagSequence'][0])
-
-    def close(self):
-        self.fileobj.close()
-
-    def get_samples(self):
-        return self._samples
-
-    def get_read_count(self, sample):
-        if not sample in self.fileobj:
-            raise "Invalid sample name: %s" % sample
-        count = 0
-        for region in self.fileobj[sample]:
-            count += len(self.fileobj[sample][region]['Fragments']['yxLocation'])
-        return count
-
-    def fetch(self, sample, tags=None):
-        if not tags:
-            tags = [t for t in  self.tags]
-
-        if not sample in self.fileobj:
-            raise "Invalid sample name: %s" % sample
-
-        if ETA:
-            count = 0
-            for region in self.fileobj[sample]:
-                count += len(self.fileobj[sample][region]['Fragments']['yxLocation'])
-            eta = ETA(count)
-        else:
-            eta = None
-
-        # Reads each region into memory at a time by tag. Then yields the sequences
-        # in order so that the tags are interlaced.
-        #
-        # This is slightly faster than just reading in one at a time.
-
-        n = 0
-        for i, region in enumerate(self.fileobj[sample]):
-            locations = []
-            for y, x in self.fileobj[sample][region]['Fragments']['yxLocation']:
-                locations.append((y, x))
-
-            vals = {}
-            for tag in tags:
-                vals[tag] = []
-                if self.tags[tag].is_colorspace:
-                    k = 'ColorCallQV'
-                else:
-                    k = 'BaseCallQV'
-
-                for (y, x), basequals in zip(locations, self.fileobj[sample][region][tag][k]):
-                    if eta:
-                        n += 1
-                        eta.print_status(n)
-
-                    calls = []
-                    if self.tags[tag].prefix:
-                        calls.append(self.tags[tag].prefix)
-
-                    quals = []
-                    for byte in basequals:
-                        call = str(byte & 0x03)
-                        qual = byte >> 2
-
-                        if qual == 63:
-                            call = '.'
-                            qual = 0
-
-                        calls.append(call)
-                        quals.append(qual)
-
-                    name = '%s_%s_%s' % (int(region), y, x)
-                    if len(tags) > 1:
-                        name = name + ' %s' % (tag)
-
-                    vals[tag].append((name, ''.join(calls), quals))
-
-            for i in xrange(len(locations)):
-                for tag in tags:
-                    yield(vals[tag][i])
-        if eta:
-            eta.done()
-
-    def dump(self, key, parent=None, indent=0):
-        if parent is None:
-            parent = self.fileobj
-
-        spaces = '  ' * indent
-
-        print '%s[%s]' % (spaces, key)
-
-        if 'CLASS' in parent[key].attrs and parent[key].attrs['CLASS'] == 'TABLE':
-            headers = []
-            maxsize = []
-            for name, val in parent[key].attrs.iteritems():
-                if name[:6] == 'FIELD_' and name[-5:] == '_NAME':
-                    headers.append(val)
-                    maxsize.append(len(val))
-
-            values = []
-            for child in parent[key]:
-                values.append(child)
-
-            for valset in values:
-                for i, val in enumerate(valset):
-                    if len(str(val)) > maxsize[i]:
-                        maxsize[i] = len(str(val))
-
-            sys.stdout.write(spaces + '  ')
-            sys.stdout.write(' | ')
-            for header, size in zip(headers, maxsize):
-                sys.stdout.write(header.ljust(size))
-                sys.stdout.write(' | ')
-            sys.stdout.write('\n')
-            for valset in values:
-                sys.stdout.write(spaces + '  ')
-                sys.stdout.write(' | ')
-                for val, size in zip(valset, maxsize):
-                    sys.stdout.write(str(val).ljust(size))
-                    sys.stdout.write(' | ')
-                sys.stdout.write('\n')
-
-        else:
-            if parent[key].attrs:
-                for name, val in parent[key].attrs.iteritems():
-                    print '%s  %s: %s' % (spaces, name, val)
-
-            for child in parent[key]:
-                self.dump(child, parent[key], indent + 1)
-
+from xsqutils import XSQFile
 
 def xsq_list(filename, count=False):
     xsq = XSQFile(filename)
@@ -190,14 +37,17 @@ def xsq_info(filename):
     xsq.close()
 
 
-def xsq_convert(filename, sample=None, tags=None):
+def xsq_convert(filename, sample=None, tags=None, suffix=None):
     xsq = XSQFile(filename)
     for name, seq, quals in xsq.fetch(sample, tags):
-        sys.stdout.write('@%s\n%s\n+\n%s\n' % (name, seq, ''.join([chr(q + 33) for q in quals])))
+        if suffix:
+            sys.stdout.write('@%s%s\n%s\n+\n%s\n' % (name, suffix, seq, ''.join([chr(q + 33) for q in quals])))
+        else:
+            sys.stdout.write('@%s\n%s\n+\n%s\n' % (name, seq, ''.join([chr(q + 33) for q in quals])))
     xsq.close()
 
 
-def xsq_convert_all(filename, tags=None, force=False):
+def xsq_convert_all(filename, tags=None, force=False, suffix=None, noz=False):
     xsq = XSQFile(filename)
     for sample in xsq.get_samples():
         sys.stderr.write('Sample: %s... ' % sample)
@@ -207,9 +57,16 @@ def xsq_convert_all(filename, tags=None, force=False):
 
         if force or not os.path.exists(outname):
             sys.stderr.write('\n')
-            out = gzip.open(tmpname, 'w')
+            if noz:
+                out = gzip.open(tmpname, 'w')
+            else:
+                out = open(tmpname, 'w')
+                
             for name, seq, quals in xsq.fetch(sample, tags):
-                out.write('@%s\n%s\n+\n%s\n' % (name, seq, ''.join([chr(q + 33) for q in quals])))
+                if suffix:
+                    out.write('@%s%s\n%s\n+\n%s\n' % (name, suffix, seq, ''.join([chr(q + 33) for q in quals])))
+                else:
+                    out.write('@%s\n%s\n+\n%s\n' % (name, seq, ''.join([chr(q + 33) for q in quals])))
             out.close()
             os.rename(tmpname, outname)
         else:
@@ -228,25 +85,26 @@ Commands:
           -c        Show the number of reads present for each tag
     convert   - Converts XSQ samples and fragments to FASTQ format
         Options:
-          -a        Convert all samples (saves to sample_name.fastq.gz)
-          -f        Overwrite existing files
-          -n name   Convert only sample "name" (writes to stdout)
-                    (can be only one)
-          -t tag    Convert only this tag (can be more than one)
-                    If more than one tag is given, the sequences for
-                    each read will be written out together.
-
-                      For example:
-                      @read1 F3
-                      F3 seq
-                      +
-                      F3 qual
-                      @read1 R5
-                      R5 seq
-                      +
-                      R5 qual
-                      @read2
-                      ...
+          -a           Convert all samples (saves to sample_name.fastq.gz)
+          -f           Overwrite existing files
+          -n name      Convert only sample "name" (writes to stdout)
+                       (can be only one, written uncompressed)
+          -noz         Don't compress the output FASTQ files with gzip
+          -s suffix    Append a suffix to all read names
+          -t tag       Convert only this tag (can be more than one)
+                       If more than one tag is given, the sequences for
+                       each read will be written out together.
+                          For example:
+                          @read1 F3
+                          F3 seq
+                          +
+                          F3 qual
+                          @read1 R5
+                          R5 seq
+                          +
+                          R5 qual
+                          @read2
+                          ...
 
         The default is to convert all samples and all fragments/tags.
 '''
@@ -262,6 +120,8 @@ if __name__ == '__main__':
     last = None
     force = False
     count = False
+    suffix = None
+    noz = False
 
     for arg in sys.argv[1:]:
         if not cmd and arg in ['list', 'convert', 'info']:
@@ -269,11 +129,16 @@ if __name__ == '__main__':
         elif last == '-n':
             sample_name = arg
             last = None
+        elif last == '-s':
+            suffix = arg
+            last = None
         elif last == '-t':
             tags .append(arg)
             last = None
-        elif arg in ['-t', '-n']:
+        elif arg in ['-t', '-n', '-s']:
             last = arg
+        elif arg == '-noz':
+            noz = True
         elif arg == '-c':
             count = True
         elif arg == '-f':
@@ -294,9 +159,9 @@ if __name__ == '__main__':
         xsq_info(fname)
     elif cmd == 'convert':
         if all:
-            xsq_convert_all(fname, tags, force)
+            xsq_convert_all(fname, tags, force, suffix, noz)
         elif sample_name:
-            xsq_convert(fname, sample_name, tags)
+            xsq_convert(fname, sample_name, tags, suffix)
         else:
             sys.stderr.write('Missing argument! Must specify "-a" or "-n sample"\n\n')
             usage()

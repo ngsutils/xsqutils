37f7eef8ab677d2817f91106f8543d1fb98d8e52
Marcus Breese <mbreese@iupui.edu>
2012-09-25 17:08:43 -0400

    added makefile, support older python virtualenv

--- init.sh
+++ init.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-virtualenv env
+VIRTUALENV=`which virtualenv-2.7`
+if [ "$VIRTUALENV" == "" ]; then
+    VIRTUALENV=`which virtualenv-2.6`
+fi
+if [ "$VIRTUALENV" == "" ]; then
+    VIRTUALENV=`which virtualenv`
+fi
+if [ "$VIRTUALENV" == "" ]; then
+    echo "Missing virtualenv!"
+    exit 1
+fi
+
+if [ ! -e env ]; then 
+    echo "Initializing virtualenv folder (env)"
+    $VIRTUALENV --no-site-packages env
+fi
+
 . env/bin/activate
+
 pip install numpy
 pip install cython
 pip install numexpr
-pip install tables
+pip install tables==2.3.1
